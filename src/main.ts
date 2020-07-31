cf6eef6a6441260cf3853bdbc248cd0929db9e32
Deka <dekachan16@gmail.com>
2020-07-31 15:18:17 +0900

    fix: Fix to run on node 14 -> 12

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import fs from 'fs/promises'
+import * as fs from 'fs'
 import { upload } from './upload'
 
 const accessToken = core.getInput('dropbox_access_token')
@@ -8,7 +8,7 @@ const path = core.getInput('path')
 
 async function run() {
   try {
-    const contents = await fs.readFile(file)
+    const contents = await fs.promises.readFile(file)
     core.debug(`Uploading '${file}' -> '${path}'`)
     await upload(path, contents, accessToken)
     core.info(`'${file}' -> '${path}' has been successfully uploaded`)
