36aafe9296be60283c299c29b2132eebd20c2c9d
Deka <dekachan16@gmail.com>
2020-11-25 14:30:20 +0900

    chore: Improve eslint/prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
     es6: true,
+    node: true,
     'jest/globals': true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:jest/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
   ],
-  parser: '@typescript-eslint/parser',
 }
--- src/main.ts
+++ src/main.ts
@@ -17,4 +17,4 @@ async function run() {
   }
 }
 
-run()
+void run()
