2bb80cbe50e5d8636e70cce67eb0ae30af1e8c09
Deka <dekachan16@gmail.com>
2020-12-03 21:28:57 +0900

    feat: Add parameter 'multiple' for uploading multiple files

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import globby from 'globby'
 import * as core from '@actions/core'
 import * as fs from 'fs'
-import { join } from 'path'
+import { join, basename } from 'path'
 import { makeUpload } from './upload'
 import { isDirectory } from './utils'
 
 const accessToken = core.getInput('dropbox_access_token')
 const src = core.getInput('src')
 const dest = core.getInput('dest')
+const multiple = core.getInput('multiple').toLowerCase() === 'true'
 
 const mode = core.getInput('mode')
 const autorename = core.getInput('autorename').toLowerCase() === 'true'
@@ -15,21 +16,20 @@ const mute = core.getInput('mute').toLowerCase() === 'true'
 
 async function run() {
   try {
-    const files = await globby(src)
     const { upload } = makeUpload(accessToken)
 
-    if (files.length == 1) {
-      const file = files[0]
-      const contents = await fs.promises.readFile(file)
+    if (!multiple) {
+      const contents = await fs.promises.readFile(src)
       if (isDirectory(dest)) {
-        const path = join(dest, file)
+        const path = join(dest, basename(src))
         await upload(path, contents, { mode, autorename, mute })
-        core.info(`Uploaded: ${file} -> ${path}`)
+        core.info(`Uploaded: ${src} -> ${path}`)
       } else {
         await upload(dest, contents, { mode, autorename, mute })
-        core.info(`Uploaded: ${file} -> ${dest}`)
+        core.info(`Uploaded: ${src} -> ${dest}`)
       }
     } else {
+      const files = await globby(src)
       await Promise.all(
         files.map(async (file) => {
           const path = join(dest, file)
