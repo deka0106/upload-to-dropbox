111f08e958e5900a7aff20f48c03ae8c6c3ad146
Deka <dekachan16@gmail.com>
2020-12-03 09:50:58 +0900

    feat: Add options for filesUpload

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import globby from 'globby'
 import * as core from '@actions/core'
 import * as fs from 'fs'
 import { join } from 'path'
-import { upload } from './upload'
+import { makeUpload } from './upload'
 import { isDirectory } from './utils'
 
 const accessToken = core.getInput('dropbox_access_token')
@@ -10,20 +10,23 @@ const src = core.getInput('src')
 const dest = core.getInput('dest')
 
 const mode = core.getInput('mode')
+const autorename = core.getInput('autorename').toLowerCase() === 'true'
+const mute = core.getInput('mute').toLowerCase() === 'true'
 
 async function run() {
   try {
     const files = await globby(src)
+    const { upload } = makeUpload(accessToken)
 
     if (files.length == 1) {
       const file = files[0]
       const contents = await fs.promises.readFile(file)
       if (isDirectory(dest)) {
         const path = join(dest, file)
-        await upload(path, contents, accessToken, mode)
+        await upload(path, contents, { mode, autorename, mute })
         core.info(`Uploaded: ${file} -> ${path}`)
       } else {
-        await upload(dest, contents, accessToken, mode)
+        await upload(dest, contents, { mode, autorename, mute })
         core.info(`Uploaded: ${file} -> ${dest}`)
       }
     } else {
@@ -31,7 +34,7 @@ async function run() {
         files.map(async (file) => {
           const path = join(dest, file)
           const contents = await fs.promises.readFile(file)
-          await upload(path, contents, accessToken, mode)
+          await upload(path, contents, { mode, autorename, mute })
           core.info(`Uploaded: ${file} -> ${path}`)
         })
       )
--- src/upload.ts
+++ src/upload.ts
@@ -1,16 +1,32 @@
 import { Dropbox, files } from 'dropbox'
 import fetch from 'node-fetch'
 
-export async function upload(
-  path: string,
-  contents: Buffer,
-  accessToken: string,
-  dropboxMode: string
-): Promise<void> {
-  const mode = getMode(dropboxMode)
-
+export function makeUpload(
+  accessToken: string
+): {
+  upload: (
+    path: string,
+    contents: Buffer,
+    options: {
+      mode: string
+      autorename: boolean
+      mute: boolean
+    }
+  ) => Promise<void>
+} {
   const dropbox = new Dropbox({ accessToken, fetch })
-  await dropbox.filesUpload({ path, contents, mode })
+
+  return {
+    upload: async (path, contents, options) => {
+      await dropbox.filesUpload({
+        path,
+        contents,
+        mode: getMode(options.mode),
+        autorename: options.autorename,
+        mute: options.mute,
+      })
+    },
+  }
 }
 
 function getMode(mode: string): files.WriteMode {
